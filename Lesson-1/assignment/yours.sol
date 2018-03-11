5e96c7b6d62d323a8e31da1c7dca84d3b2f5c67a
Ze Li <lize1985@gmail.com>
2018-03-11 04:15:49 -0700

    Update yours.sol

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,57 @@
 /*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint salary;
+    address receiver;
+    address owner;
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now;
+    
+    // Assume that the contract who created is the owner of the Payroll
+    function Payroll() payable {
+        owner = msg.sender;
+    }
+    
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    // Assume that only the owner of the payroll system can set the salary
+    function setSalary() payable returns(uint){
+        if(msg.sender != owner) {
+            revert();
+        }
+        salary = msg.value;
+        return salary;
+    }
+    
+    function registerReceiver() returns(address){
+        receiver = msg.sender;
+        return receiver;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool){
+        return calculateRunway() >0;
+    }
+
+    function getPaid() {
+        if(msg.sender != receiver) {
+            revert();
+        }
+        
+        uint nextPayDay = lastPayday + payDuration;
+        if ( nextPayDay> now) {
+            revert();
+        }
+        
+        lastPayday = lastPayday + payDuration;
+        receiver.transfer(salary);
+    }    
+    
+}
